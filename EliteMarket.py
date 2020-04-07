9f2af18ebd169ee2536af93dd36c0ad4a978dc6c
AweBob <33764800+AweBob@users.noreply.github.com>
2020-04-06 18:15:45 -0700

    Removed old algorithm, implemented reading of Cargo.json

--- EliteMarket.py
+++ EliteMarket.py
@@ -1,8 +1,14 @@
 
-amountInHold = int(input("How much do you have in your hold?: "))
+from EliteExtraJsonParser import ExtraJsonParserMain #for reading Cargo.json
+import requests #For pinging inara
+from bs4 import BeautifulSoup #for sorting inara
 
-import requests
-from bs4 import BeautifulSoup
+json_Cargo , json_Market , json_ModulesInfo , json_Outfitting , json_Shipyard , json_Status = ExtraJsonParserMain() #Get all the 
+
+amountInHold = json_Cargo["Count"]
+for item in json_Cargo["Inventory"] :
+    if ( item["Name_Localised"] == "Limpet" ) :
+        amountInHold = amountInHold - item["Count"]
 
 url='https://inara.cz/ajaxaction.php?act=goodsdata&refname=sellmax&refid=144&refid2=0' #get this link from inara inspect element
 params ={}
@@ -16,7 +22,7 @@ body = list(html.children)[1] #In the body tag
 table = list(body.children)[1] #In the table tag
 tbody = list(table.children)[1] #In the tbody tag
 
-trsOnPage = 40 #This is the number of items that r in the table (rows), or in the thml the number of tr headers
+trsOnPage = 40 #This is the number of items that are in the table (rows), or in the thml the number of tr headers
 
 stationList = [] #str
 systemList = [] #Str
@@ -28,7 +34,6 @@ payoutOfHundredPercentList = [] #float
 actualPriceList = [] #float
 
 for i in range(0,trsOnPage) :
-
     tr =  list(tbody.children)[i]
     tdName = list(tr.children)[0]
     span = list(tdName.children)[0]
@@ -41,7 +46,6 @@ for i in range(0,trsOnPage) :
     quantity = list(tr.children)[4].get_text()
     price = list(tr.children)[5].get_text()
     time = list(tr.children)[6].get_text()
-
     stationList.append(station)
     systemList.append(system)
     largePadList.append(pad == "L") #If pad is large append True, if it isn't as in medium "M" 
@@ -49,22 +53,5 @@ for i in range(0,trsOnPage) :
     priceList.append(int( price.replace(",","")[:-3] )) #remove commas, remove last three characters " Cr" and convert to int
     timeList.append(time)  
     
-    if ( int(quantity.replace(",","")) > amountInHold * 27.7777 ) :
-        payoutOfHundredPercentTemp = 100
-    else :
-        payoutOfHundredPercentTemp = (2.8527 * (int(quantity.replace(",","")) / int(amountInHold) ) ) + 46.8285
-        if ( payoutOfHundredPercentTemp > 100 ) :
-            payoutOfHundredPercentTemp = 100
-
-    payoutOfHundredPercentList.append( payoutOfHundredPercentTemp ) #payout of 100% = 2.8527 * (Demand/Units) + 46.8285
-    actualPriceList.append( payoutOfHundredPercentTemp * int( price.replace(",","")[:-3] ) )
-
-bigestVal = 0
-bigestValIndex = 0
-for i in range(0,trsOnPage) :
-    if ( actualPriceList[i] > bigestVal) :
-        bigestVal = actualPriceList[i]
-        bigestValIndex = i
-
-print("The best system to sell at's estimated price is " + str(actualPriceList[bigestValIndex]) + " with a payout of 100 percent of " + str(payoutOfHundredPercentList[bigestValIndex]))
-print(str(i) + ". " + station + " | " + system + " | " + pad + " | " + scDist + " " + dist + " | " + quantity + " | " + price + " | " + time)
+#Implement neotron's estimation algorithm
+#Printout top 3 systems to sell at as some may be in Colonia
