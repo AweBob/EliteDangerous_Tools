07724bd20a697d633a8f89ee59bda609147c3575
AweBob <33764800+AweBob@users.noreply.github.com>
2020-04-07 21:50:09 -0700

    Removed Unnecessary list & preparing groupValue() to printout results

--- LtdMarket.py
+++ LtdMarket.py
@@ -58,7 +58,90 @@ def groupValue( tonsList ) : #prints results for a maximum of 4 users, displays 
 
     print("Computing for " + str(averageTonsAboard) + " tons of LTDs which is the average of " + str(stringTonsList) + "." )
 
+    url='https://inara.cz/ajaxaction.php?act=goodsdata&refname=sellmax&refid=144&refid2=0' #get this link from inara inspect element - THIS IS THE LINK FOR LOW TEMPATURE DIAMONDS
+    params ={}
+    response=requests.post(url, data=params)
+    #print(response.status_code) #200 is good
 
+    soup = BeautifulSoup(response.text, 'html5lib')
+
+    html = list(soup.children)[0] #In the html tag
+    body = list(html.children)[1] #In the body tag
+    table = list(body.children)[1] #In the table tag
+    tbody = list(table.children)[1] #In the tbody tag
+
+    trsOnPage = 40 #This is the number of items that are in the table (rows), or in the html the number of tr headers
+
+    stationList = [] #str
+    systemList = [] #Str
+    largePadList = []  #str
+    quantityList = [] #Int
+    priceList = [] #Int
+    timeList = [] #Str
+
+    for i in range(0,trsOnPage) :
+        tr =  list(tbody.children)[i]
+        tdName = list(tr.children)[0]
+        span = list(tdName.children)[0]
+        a = list(span.children)[1]
+        station = list(a.children)[0].get_text()
+        system = list(a.children)[2].get_text()
+        pad = list(tr.children)[1].get_text()
+        dist = list(tr.children)[3].get_text()
+        quantity = list(tr.children)[4].get_text()
+        price = list(tr.children)[5].get_text()
+        time = list(tr.children)[6].get_text()
+        if (float(dist[:-3]) <  10000) : #exclude colonia ones or ones more than 10,000 lys out 
+            stationList.append(station)
+            systemList.append(system)
+            largePadList.append(pad) #str
+            quantityList.append( int(quantity.replace(",","")) ) #Remove the commas then convert to integer
+            priceList.append(int( price.replace(",","")[:-3] )) #remove commas, remove last three characters " Cr" and convert to int
+            timeList.append(time)  
+        
+    #=================================================================================================================================
+    #==================CREDIT TO: https://github.com/neotron aka CMDR Neotron for the aproximation algorithm below====================
+    #=================================================================================================================================
+    estimatedPricePerTonList = []
+    perton = 0.00215 #0.215% per ton
+    maxratio = 27.77777777777777
+    #averageTonsAboard set above
+    for i in range(0, len(systemList)) :
+        demand = quantityList[i] 
+        cost = priceList[i] 
+        perton = perton / ( demand / 504 )
+        reduction = 0
+        for i in range(0, averageTonsAboard) :
+            ratio = demand / (averageTonsAboard - i)
+            if (ratio <= maxratio) :
+                reduction = reduction + perton
+        reduction = min(0.7674, reduction)
+        estimatedPricePerTonList.append( cost*(1-reduction) )
+    #=================================================================================================================================
+    #=================================================================================================================================
+    #=================================================================================================================================
+
+    numValues = 5 #I want 5 results to show (including the header)
+
+    bigestIndexes = [0]
+    bigestValues = [0]
+    for i in range(1,numValues + 1) :
+        bigestVal = max(estimatedPricePerTonList)
+        index = estimatedPricePerTonList.index( bigestVal ) 
+        estimatedPricePerTonList[index] = -1 #so it won't get chosen for the next bigest val
+        bigestIndexes.append(index)
+        bigestValues.append(bigestVal)
+
+    if ( len(tonsList) == 2 ) :
+        printoutInfo = [ ["Rank", "≈Price Per Ton #1", "≈Total Price #1", "≈Trade Dividends #1", "≈Price Per Ton #2", "≈Total Price #2", "≈Trade Dividends #2", "≈Trade Dividends #3", "System", "Station", "Pad", "Last Updated"] ] #initialize it with the header
+
+    elif ( len(tonsList) == 3 ) :
+        printoutInfo = [ ["Rank", "≈Price Per Ton #1", "≈Total Price #1", "≈Trade Dividends #1", "≈Price Per Ton #2", "≈Total Price #2", "≈Trade Dividends #2", "≈Price Per Ton #3", "≈Total Price #3", "≈Trade Dividends #3", "≈Trade Dividends #4", "System", "Station", "Pad", "Last Updated"] ] #initialize it with the header
+
+    else : # ( len(tonsList) == 4 ) <-- is the only other possible thing
+        printoutInfo = [ ["Rank", "≈Price Per Ton #1", "≈Total Price #1", "≈Trade Dividends #1", "≈Price Per Ton #2", "≈Total Price #2", "≈Trade Dividends #2", "≈Price Per Ton #3", "≈Total Price #3", "≈Trade Dividends #3", "≈Price Per Ton #4", "≈Total Price #4", "≈Trade Dividends #4", "System", "Station", "Pad", "Last Updated"] ] #initialize it with the header
+
+    print(str(printoutInfo[0]))
 
     main()
 
@@ -85,7 +168,6 @@ def singleValue( tonsAboard ) : #prints results for one user - print trade divid
     stationList = [] #str
     systemList = [] #Str
     largePadList = []  #str
-    distanceList = [] #Str
     quantityList = [] #Int
     priceList = [] #Int
     timeList = [] #Str
@@ -106,7 +188,6 @@ def singleValue( tonsAboard ) : #prints results for one user - print trade divid
             stationList.append(station)
             systemList.append(system)
             largePadList.append(pad) #str
-            distanceList.append(dist) #string for printing later
             quantityList.append( int(quantity.replace(",","")) ) #Remove the commas then convert to integer
             priceList.append(int( price.replace(",","")[:-3] )) #remove commas, remove last three characters " Cr" and convert to int
             timeList.append(time)  
