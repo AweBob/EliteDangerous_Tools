f65f6c5574c8fc0270b3890d2b3f4e03258253f7
AweBob <33764800+AweBob@users.noreply.github.com>
2020-11-18 10:19:06 -0800

    Combined MarketAnalysis2 into MarketAnalysis

--- MarketAnalysis.py
+++ MarketAnalysis.py
@@ -1,15 +1,23 @@
 
-import requests #
+import requests #For apis
 import numpy as np #for distance calculation 
+import time
 
 #If you get errors on the above 2 things run: 
 # py -m pip install requests
 # py -m pip install numpy
 
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
 #================================Credit for this idea to CMDR Longman.P.J.===========================================================
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
 
-def systemsThatMatchActiveStates (doStatusPrintout) : 
-    eddbGrab = eddbSystems
+#This works for sure and is quick, but not very clean code
+
+def systemsThatMatchActiveStates (doStatusPrintout, eddbGrab) :
     systems = []
     systemsCount = len(eddbGrab)
     if doStatusPrintout :
@@ -39,58 +47,21 @@ def systemsThatMatchActiveStates (doStatusPrintout) :
         print("") #get a newline
     return(systems, systemsCount)  #systems is [[sysname, sysid], [], []]  sysCount is the number of systems downloaded from eddb and analyzed for these checks
 
-#====================================================================================================================================
-
-def stationsThatMatchEconomy (rawSystemList) : 
+def stationsThatMatchEconomy (rawSystemList, eddbGrab, eddbGrabSys) : 
     systemEddbIds = []
     for rawSystem in rawSystemList :
         systemEddbIds.append(rawSystem[1]) 
-    eddbGrab = eddbStations
     stations = []
-    #stationsCount = len(eddbGrab)
     for station in eddbGrab :
         if station["system_id"] in systemEddbIds :
             economiesList = station["economies"]
             for economy in economiesList :
                 if economy=="Industrial" or economy=="High Tech" or economy=="Tourism" :  #Industrial, High Tech, Refinery or Tourism station enconomies required
-                    stations.append([ station["name"], getSystemName(station["system_id"]), station["id"], station["system_id"] ]) #running get system name every time is ineficient asf but whatever lol
+                    stations.append([ station["name"], getSystemName(station["system_id"], eddbGrabSys), station["id"], station["system_id"] ]) #running get system name every time is ineficient asf but whatever lol
                     break
     return(stations) #[[stationame,sysname,statioid,sysid], [], []]
 
-#====================================================================================================================================
-
-def getSystemCordinatesFromId(systemEddbId) :
-    eddbGrab = eddbSystems
-    for systemDict in eddbGrab :
-        if systemDict["id"]==systemEddbId :
-            return (True, [systemDict["x"], systemDict["y"], systemDict["z"]])
-    return (False,[]) 
-
-def getSystemCordinatesFromName(systemName) :
-    eddbGrab = eddbSystems
-    for systemDict in eddbGrab :
-        if systemDict["name"]==systemName :
-            return (True, [systemDict["x"], systemDict["y"], systemDict["z"]])
-    return (False,[]) 
-
-def getSystemDistance(sys1CordinateList, sys2CordinateList) : #cordinate list looks like [xCord, yCord, zCord] all in integer form
-    p1 = np.array(sys1CordinateList)
-    p2 = np.array(sys2CordinateList)
-    squared_dist = np.sum((p1-p2)**2, axis=0)
-    dist = np.sqrt(squared_dist)
-    return(dist)
-
-def getSystemNames(systemIdList) :
-    systemNames = []
-    eddbGrab = eddbSystems
-    for system in eddbGrab :
-        for inputId in systemIdList :
-            if inputId==system["id"] :
-                systemNames.append(system["name"])
-    return(systemNames)
-
-def getSystemName(sysId) :
-    eddbGrab = eddbSystems
+def getSystemName(sysId, eddbGrab) : 
     for system in eddbGrab :
         if sysId==system["id"] :
             return(system["name"])
@@ -106,14 +77,10 @@ def doEddbPings() :
     eddbGrab2 = response.json()
     return(eddbGrab1, eddbGrab2)
 
-#====================================================================================================================================
-
-if __name__ == "__main__" :
-
+def marketAnalysis1 () :
     eddbSystems, eddbStations = doEddbPings()
-
-    rawSystemsList, systemsCount = systemsThatMatchActiveStates(False) #rawSystemList =  [[sysname, sysid], [], []]
-    rawStationsList = stationsThatMatchEconomy(rawSystemsList) #rawStationsList = [[stationame,sysname,statioid,sysid], [], []]
+    rawSystemsList, systemsCount = systemsThatMatchActiveStates(False, eddbSystems) #rawSystemList =  [[sysname, sysid], [], []]
+    rawStationsList = stationsThatMatchEconomy(rawSystemsList, eddbStations, eddbSystems) #rawStationsList = [[stationame,sysname,statioid,sysid], [], []]
     print("\nAnalyzed " + str(systemsCount) + " systems. States: public holiday, civil liverty, expansion, boom or investment  Economy: Industrial High Tech Tourism")
     if len(rawStationsList)==0:
         print("not a single system meets the requirements F")
@@ -121,3 +88,226 @@ if __name__ == "__main__" :
         print(rawStationsList[i][0] + ", " + rawStationsList[i][1])
 
 #====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+
+#Market Analysis 2: This one takes an extremely longass time
+
+class EddbSystem :
+    def __init__(self, eddbSystemDictionary) :
+        self.id = eddbSystemDictionary["id"]
+        self.name = eddbSystemDictionary["name"]
+        self.x = eddbSystemDictionary["x"]
+        self.y = eddbSystemDictionary["y"]
+        self.z = eddbSystemDictionary["z"]
+
+    def getId(self) :
+        return(self.id)
+    def getName(self) :
+        return(self.name)
+    def getX(self) :
+        return(self.x)
+    def getY(self) :
+        return(self.y)
+    def getZ(self) :
+        return(self.z)
+    
+    def getDistance(self, targetSystem) : 
+        p1 = np.array([float(self.x), float(self.y), float(self.z)])
+        p2 = np.array([float(targetSystem.getX()), float(targetSystem.getY()), float(targetSystem.getZ())])
+        squared_dist = np.sum((p1-p2)**2, axis=0)
+        dist = np.sqrt(squared_dist)
+        return(dist)
+
+    def getDistanceFromSol(self) :
+        p1 = np.array([float(self.x), float(self.y), float(self.z)])
+        p2 = np.array([0,0,0])
+        squared_dist = np.sum((p1-p2)**2, axis=0)
+        dist = np.sqrt(squared_dist)
+        return(dist)
+
+    def hasFourLuckyStates(self) :
+        states = []
+        systemInfo = requests.get( f"https://elitebgs.app/api/ebgs/v4/systems?name={self.name}"  ).json()["docs"]
+        for bgsState in systemInfo :
+            states.append( bgsState["name"] )
+        ibe = False #investement/boom
+        cle = False #civil liberty
+        phe = False #public holiday
+        ee = False #expansion
+        pae = False #pirate attack #not used currently but if you wanted to experiment
+        for state in states :
+            if (state=="Boom" or state=="Investment"):
+                ibe = True
+            elif (state=="Civil Liberty"):
+                cle = True
+            elif (state=="Public Holiday"):
+                phe = True
+            elif (state=="Pirate Attack") :
+                pae = True
+            elif (state=="Expansion"):
+                ee = True
+        return(ibe and cle and ee and phe)
+
+    def getStationsWithLuckyEconomy(self) :
+        returningStations = []
+        stationsList = requests.get( f"https://elitebgs.app/api/ebgs/v4/stations?eddbid={self.id}"  ).json()["docs"]
+        for stationDict in stationsList :
+            if stationDict["economy"]=="$economy_refinery;" or stationDict["economy"]=="$economy_hightech;" or stationDict["economy"]=="$economy_industrial;" or stationDict["economy"]=="$economy_tourism;" :  #Industrial, High Tech, Refinery or Tourism station enconomies required
+                returningStations.append(BasicStation(self.name, stationDict["name"]))
+        return(returningStations)
+
+#====================================================================================================================================
+
+class BasicStation :
+    def __init__(self, sysName, statName) :
+        self.sysName = sysName
+        self.statName = statName
+    def getSys(self) :
+        return(self.sysName)
+    def getStat(self) :
+        return(self.getStat)
+
+#====================================================================================================================================
+
+def marketAnalysis2 () :
+    input("Market Analysis 2 - By: AweBob#6221 - Press enter to begin - ")
+
+    print("Downloading eddb populated systems")
+    rawEddbPopulatedSystems = requests.get( "https://eddb.io/archive/v6/systems_populated.json" ).json()
+    
+    print("Parsing Data")
+    eddbSystems = []
+    for rawEddbSystemDictionary in rawEddbPopulatedSystems :
+        eddbSystems.append( EddbSystem(rawEddbSystemDictionary) )
+
+    """
+    print("Filtering Colonia Systems") #Not Tested
+    for eddbSystem in eddbSystems :
+        if eddbSystem.getDistanceFromSol() > 10000 :
+            eddbSystems.pop(eddbSystem)
+    """
+
+    print("Processing Individual Systems\n")
+    startTime = int(time.time())
+    for index, eddbSystem in enumerate(eddbSystems) :
+        if eddbSystem.hasFourLuckyStates() :
+            for station in eddbSystem.getStationsWithLuckyEconomy() :
+                print(f"\n{station.getSys()} - {station.getStat()} - {int(eddbSystem.getDistanceFromSol())}lys\n")
+
+        """
+        #Used this to get my time estimate for how long this would take
+        try :
+            print(f"Processed {index} out of {len(eddbSystems)} in {int(time.time()) - startTime} secs projecting finished in {int(((int(time.time()) - startTime) /  index ) * len(eddbSystems))} secs")
+        except ZeroDivisionError:
+            print(f"Processed {index} out of {len(eddbSystems)} in {int(time.time()) - startTime} secs projecting finished in ? secs")
+        """
+
+    print(f"\nDone in {int(time.time()) - startTime} secs")
+
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+
+#This will work the same way market analysis1 does but its going to be a lot cleaner once I'm done with it
+
+class AdvancedEddbSystem :
+    def __init__(self, eddbSystemDictionary) :
+        self.id = eddbSystemDictionary["id"]
+        self.name = eddbSystemDictionary["name"]
+        self.x = eddbSystemDictionary["x"]
+        self.y = eddbSystemDictionary["y"]
+        self.z = eddbSystemDictionary["z"]
+        self.stations = [] #These are added via self.addStation()
+        self.states = []
+        for state in eddbSystemDictionary["states"] :
+            self.states.append(state["name"])
+
+    def addStation(self, station) :
+        self.stations.append([ station["name"], station["economies"] ])
+
+    def getId(self) :
+        return(self.id)
+    def getName(self) :
+        return(self.name)
+    def getX(self) :
+        return(self.x)
+    def getY(self) :
+        return(self.y)
+    def getZ(self) :
+        return(self.z)
+    
+    def getDistance(self, targetSystem) : 
+        p1 = np.array([float(self.x), float(self.y), float(self.z)])
+        p2 = np.array([float(targetSystem.getX()), float(targetSystem.getY()), float(targetSystem.getZ())])
+        squared_dist = np.sum((p1-p2)**2, axis=0)
+        dist = np.sqrt(squared_dist)
+        return(dist)
+
+    def getDistanceFromSol(self) :
+        p1 = np.array([float(self.x), float(self.y), float(self.z)])
+        p2 = np.array([0,0,0])
+        squared_dist = np.sum((p1-p2)**2, axis=0)
+        dist = np.sqrt(squared_dist)
+        return(dist)
+
+    def hasFourLuckyStates(self) :
+        return ("placeholder")
+
+    def getStationsWithLuckyEconomy(self) :
+        return ("placeholder")
+
+def marketAnalysis3() :
+    input("Market Analysis - By: AweBob#6221 - Press enter to begin - ")
+
+    print("Downloading eddb")
+    rawEddbPopulatedSystems = requests.get( "https://eddb.io/archive/v6/systems_populated.json" ).json()
+    rawEddbStations = requests.get( "https://eddb.io/archive/v6/stations.json" ).json()
+    
+    print("Parsing Data")
+    eddbSystems = []
+    for rawEddbSystemDictionary in rawEddbPopulatedSystems :
+        eddbSystems.append( AdvancedEddbSystem(rawEddbSystemDictionary) )
+    for rawEddbStationDictionary in rawEddbStations :
+        for eddbSystem in eddbSystems :
+            if rawEddbStationDictionary["system_id"] == eddbSystem.getId() :
+                eddbSystem.addStation(rawEddbStationDictionary)
+                break #onto next station
+
+    """
+    print("Filtering Colonia Systems") #Not Tested
+    for eddbSystem in eddbSystems :
+        if eddbSystem.getDistanceFromSol() > 10000 :
+            eddbSystems.pop(eddbSystem)
+    """
+
+    print("Processing Individual Systems\n")
+    startTime = int(time.time())
+    for eddbSystem in eddbSystems :
+        if eddbSystem.hasFourLuckyStates() :
+            for station in eddbSystem.getStationsWithLuckyEconomy() :
+                #print(f"\n{station.getSys()} - {station.getStat()} - {int(eddbSystem.getDistanceFromSol())}lys\n")
+                print("placeholder")
+
+    print(f"\nDone in {int(time.time()) - startTime} secs")
+
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+
+if __name__ == "__main__":
+    marketAnalysis1() #Ugly code but it works 100%
+    #marketAnalysis2() #Uses latest information, but takes forever to run
+    #marketAnalysis3() #Not completed yet
