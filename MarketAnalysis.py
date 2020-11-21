6de06332e1505b0af8691f46c52368169d7246b2
AweBob <33764800+AweBob@users.noreply.github.com>
2020-11-21 15:00:14 -0800

    Modified output information.

--- MarketAnalysis.py
+++ MarketAnalysis.py
@@ -1,7 +1,5 @@
 
 #====================================================================================================================================
-#====================================================================================================================================
-#====================================================================================================================================
 
 import requests #For apis
 import numpy as np #for distance calculation 
@@ -18,13 +16,16 @@ import time #For timing how long all this junk takes
 def marketAnalysis() :
     input("Market Analysis 5 - By: AweBob#6221 & Pete#1168 - Press enter to begin - ")
     
-    print("Downloading EDDB Stations")
-    startTime = int(time.time())
+    print("Downloading EDDB Stations & Populated Systems")
+    startTime = time.time()
     rawEddbStations = requests.get( "https://eddb.io/archive/v6/stations.json" ).json()
     rawEddbPopulatedSystems = requests.get( "https://eddb.io/archive/v6/systems_populated.json" ).json()
+    print(f"Download Took {round(time.time() - startTime, 2)} Seconds")
 
-    print(f"Filtering {len(rawEddbStations)} stations - {int(time.time()) - startTime}  seconds elapsed so far")
+    print(f"Filtering {len(rawEddbStations)} Stations By State & Economy")
+    startTime = time.time()
     outputStations = [[],[],[],[]]
+    pointSol = np.array([0,0,0])
     for rawEddbStation in rawEddbStations :
         economies = rawEddbStation["economies"] #economy=="Industrial" or economy=="High Tech" or economy=="Tourism"
         ind = False #industrial
@@ -70,35 +71,33 @@ def marketAnalysis() :
         if (ind or hte or tou or ref) and (not ext) : #It's an acceptable economy
             if (inv and cle and phe) or (inv and cle and pae) or (bom and cle and phe) or (bom and cle and pae) : #if its an acceptable state
                 sysName = "?" #gets overriden with the actual if it is found
+                distanceToSol = "?" #^
                 for system in rawEddbPopulatedSystems :
                     if rawEddbStation["system_id"]==system["id"] :
                         sysName = system["name"]
+                        pointSys = np.array([system["x"], system["y"], system["z"]])
+                        squared_dist = np.sum((pointSol-pointSys)**2, axis=0)
+                        distanceToSol = str(round(np.sqrt(squared_dist), 2))
                         break #No need to keep searching
                 if inv and cle and phe :
-                    outputStations[0].append([str(rawEddbStation["name"]), sysName, str( time.strftime( "%H:%M:%S" , time.gmtime(startTime - int(rawEddbStation["updated_at"]) ))), cleanStates, economies ])
+                    outputStations[0].append([str(rawEddbStation["name"]), sysName, rawEddbStation["max_landing_pad_size"], str(rawEddbStation["distance_to_star"]), distanceToSol ])
                 elif inv and cle and pae :
-                    outputStations[1].append([str(rawEddbStation["name"]), sysName, str( time.strftime( "%H:%M:%S" , time.gmtime(startTime - int(rawEddbStation["updated_at"]) ))), cleanStates, economies ])
+                    outputStations[1].append([str(rawEddbStation["name"]), sysName, rawEddbStation["max_landing_pad_size"], str(rawEddbStation["distance_to_star"]), distanceToSol ])
                 elif bom and cle and phe :
-                    outputStations[2].append([str(rawEddbStation["name"]), sysName, str( time.strftime( "%H:%M:%S" , time.gmtime(startTime - int(rawEddbStation["updated_at"]) ))), cleanStates, economies ])
+                    outputStations[2].append([str(rawEddbStation["name"]), sysName, rawEddbStation["max_landing_pad_size"], str(rawEddbStation["distance_to_star"]), distanceToSol ])
                 elif bom and cle and pae :
-                    outputStations[3].append([str(rawEddbStation["name"]), sysName, str( time.strftime( "%H:%M:%S" , time.gmtime(startTime - int(rawEddbStation["updated_at"]) ))), cleanStates, economies ])
+                    outputStations[3].append([str(rawEddbStation["name"]), sysName, rawEddbStation["max_landing_pad_size"], str(rawEddbStation["distance_to_star"]), distanceToSol ])
+    print(f"Filtering Took {round(time.time() - startTime, 2)} Seconds")
 
-    print(f"Printing final results - {int(time.time()) - startTime}  seconds elapsed so far\n")
+    print(f"Printing Final Results\n")
     for index, stationQualityLevel in enumerate(outputStations) :
         for outputStation in stationQualityLevel :
-            economyStr = ", ".join(outputStation[4])
-            statesStr = ", ".join(outputStation[3])
-            print(str(index + 1) + ". " + outputStation[0] + " - " + outputStation[1] + " - " + economyStr + " - " + statesStr ) #Add the time stuff later, that would be a nice feature
-        
-    print(f"\nProcess took {int(time.time()) - startTime} seconds")
+            print(str(index + 1) + ". " + outputStation[0] + "; " + outputStation[1] + "; " + outputStation[2] + " pad; " + outputStation[3] + " ls; " + outputStation[4] + " lys" )
+    print() #newline
 
-#====================================================================================================================================
-#====================================================================================================================================
 #====================================================================================================================================
 
 if __name__ == "__main__":
     marketAnalysis()
 
-#====================================================================================================================================
-#====================================================================================================================================
 #====================================================================================================================================
